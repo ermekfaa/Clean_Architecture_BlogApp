84445da5520f9df0e997fe63839498dc2304f7ae
ermekfaa <emrekafa2003@gmail.com>
2024-03-15 23:08:08 +0300

    app_palette change

--- blog_app/lib/core/theme/app_palette.dart
+++ blog_app/lib/core/theme/app_palette.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 class AppPallete {
-  static const Color backgroundColor = Color.fromRGBO(25, 24, 32, 1);
+  static const Color backgroundColor = Color.fromRGBO(26, 24, 32, 1);
   static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
   static const Color gradient2 = Color.fromRGBO(251, 109, 169, 1);
   static const Color gradient3 = Color.fromRGBO(255, 159, 124, 1);
